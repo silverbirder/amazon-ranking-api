9c03c870a874398212e6d9de5032dca7686e8674
shibamoto <silverbirder@gmail.com>
2019-03-18 22:44:51 +0900

    modify scraping

--- scraping.js
+++ scraping.js
@@ -1,24 +1,45 @@
+'use strict';
+
 const axios = require('axios');
 const cheerio = require('cheerio');
-const host = 'https://www.amazon.co.jp'
+const host = 'https://www.amazon.co.jp';
+let amazonCategoryUrl = []
 
-function getContent(html, selector) {
+function getContentAttrByList(html, selector, attr) {
     const $ = cheerio.load(html);
     const content = $(`${selector}`).map((i, el) => {
-        return $(el).attr('href')
+        return $(el).attr(attr)
     }).get();
     return content;
 }
 
-let urlList = []
-axios(`${host}/ranking?type=new-releases`).then(response => {
-    const content = getContent(response.data, '#crown-category-nav a');
-    urlList = urlList.concat(content)
-    content.forEach(path => {
-        axios(`${host}${path}`).then(response2 => {
-            const content2 = getContent(response2.data, '#zg_browseRoot ul ul li a')
-            urlList = urlList.concat(content2)
-            console.log(urlList)
-        });
+async function featchCategories(url, selector) {
+    const response = await axios(url);
+    // add host and delete not need parameter
+    return getContentAttrByList(response.data, selector, 'href').map(attr => {
+        const attrPlusHost = attr.startsWith(host) ? attr : `${host}${attr}`;
+        // a.jp/b/0000/c-d/f -> a.jp/b/000/
+        return attrPlusHost.replace(/\d+\//, '')
     })
-})
+}
+
+async function start(url, selector, counter) {
+    if (counter == 0) {
+        url = `${host}/ranking?type=top-sellers`
+        selector = '#crown-category-nav > a'
+    }
+    const categories = await featchCategories(url, selector)
+    amazonCategoryUrl = amazonCategoryUrl.concat(categories)
+    if (counter >= 1) {
+        console.log(amazonCategoryUrl)
+        console.log('end loop')
+        // oh.. can't access page to need one step
+        return;
+    }
+    categories.forEach(url => {
+        counter++;
+        start(url, '#zg_browseRoot > ul > ul > li > a', counter)
+    })
+}
+
+start(null, null, 0)
