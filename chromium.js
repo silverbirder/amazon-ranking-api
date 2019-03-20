00b7f639795cc5382fa9413b12019af0252b1efb
shibamoto <silverbirder@gmail.com>
2019-03-21 00:17:34 +0900

    get amazon ranking data

--- chromium.js
+++ chromium.js
@@ -2,19 +2,40 @@ const chrome = require('chrome-aws-lambda');
 // const puppeteer = require('puppeteer-core');
 const puppeteer = require('puppeteer');
 
-async function getScreenshot(url, type, quality, fullPage) {
+async function getScreenshot(url, selector) {
     // const browser = await puppeteer.launch({
     //     args: chrome.args,
     //     executablePath: await chrome.executablePath,
     //     headless: chrome.headless,
     // });
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const browser = await puppeteer.launch({args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--lang=ja,en-US;q=0.9,en;q=0.8',
+            '--user-agent=MMozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
+        ]});
 
     const page = await browser.newPage();
-    await page.goto(url);
-    const file = await page.screenshot({ type,  quality, fullPage });
+    await page.setRequestInterception(true);
+    page.on('request', interceptedRequest => {
+        if (interceptedRequest.url().endsWith('.png') || interceptedRequest.url().endsWith('.jpg'))
+            interceptedRequest.abort();
+        else
+            interceptedRequest.continue();
+    });
+    page.setDefaultNavigationTimeout(30000);
+    const response = await page.goto(url);
+    await page.waitFor(1000);// 1秒待つ
+    if (response.status() !== 200) {
+        return response.status();
+    }
+    var list = await page.$$(selector);
+    var datas = [];
+    for (let i = 0; i < list.length; i++) {
+        datas.push(await (await list[i].getProperty('textContent')).jsonValue())
+    }
     await browser.close();
-    return file;
+    return datas;
 }
 
 module.exports = { getScreenshot };
--- server.js
+++ server.js
@@ -9,25 +9,10 @@ http.createServer(function (req, res) {
 }).listen(4000, '0.0.0.0');
 
 const test = async function(req, res) {
-    try {
-        const { pathname = '/', query = {} } = parse(req.url, true);
-        const { type = 'png', quality, fullPage } = query;
-        const url = getUrlFromPath(pathname);
-        const qual = getInt(quality);
-        if (!isValidUrl(url)) {
-            res.statusCode = 400;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(`<h1>Bad Request</h1><p>The url <em>${url}</em> is not valid.</p>`);
-        } else {
-            const file = await getScreenshot(url, type, qual, fullPage);
-            res.statusCode = 200;
-            res.setHeader('Content-Type', `image/${type}`);
-            res.end(file);
-        }
-    } catch (e) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
-        console.error(e.message);
-    }
+    const file = await getScreenshot('https://www.amazon.co.jp/ranking?type=top-sellers', '#crown-category-nav > a');
+    console.log(file);
+    res.statusCode = 200;
+    res.end(file.join(','))
 }
+
+process.on('unhandledRejection', console.dir);
