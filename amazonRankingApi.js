436083c19afd32dcd58ce3a38d8a6d4f56893e42
shibamoto <silverbirder@gmail.com>
2019-03-31 01:37:16 +0900

    change response json

--- amazonRankingApi.js
+++ amazonRankingApi.js
@@ -19,24 +19,27 @@ module.exports = async function (req, res) {
     const browser = await initBrowser()
     try {
         const selector = ''
-        const data = await getAmazonCategoryUrl(browser, amazonCategoryUrl, selector, evalGetTopFunc)
+        let data = await getAmazonCategoryUrl(browser, amazonCategoryUrl, selector, evalGetTopFunc)
         await closeBrowser(browser)
+        data['url'] = amazonCategoryUrl
         res.statusCode = 200
         res.setHeader('Content-Type', `application/json`)
         res.end(JSON.stringify({
             'message': 'ok',
-            'data': data,
-            'url': amazonCategoryUrl
+            'data': data
         }))
     } catch (e) {
         console.log(e)
         await closeBrowser(browser)
-        res.statusCode = 200
+        res.statusCode = 500
         res.setHeader('Content-Type', `application/json`)
         res.end(JSON.stringify({
             'message': e,
-            'data': [],
-            'url': amazonCategoryUrl
+            'data': {
+                'url': amazonCategoryUrl,
+                'category': '',
+                'items': [],
+            },
         }))
     }
 }
